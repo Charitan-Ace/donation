aeaa394682e9e386626d9696bbb4f4dfee88d6f5
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-11 15:24:26 +0700

    feat: implement get by project ID listener

--- src/main/java/ace/charitan/donation/config/KafkaConsumerConfig.java
+++ src/main/java/ace/charitan/donation/config/KafkaConsumerConfig.java
@@ -1,46 +1,21 @@
 package ace.charitan.donation.config;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 
 @Configuration
 class KafkaConsumerConfig {
-
-    @Autowired
-    private KafkaTemplate<String, Object> kafkaTemplate;
-
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        String bootstrapServers = System.getenv("KAFKA_BOOTSTRAP_SERVERS");
-        if (bootstrapServers == null || bootstrapServers.isEmpty()) {
-            bootstrapServers = "kafka:9092";
-        }
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "donation");
-        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "ace.charitan.*");
-        return new DefaultKafkaConsumerFactory<>(configProps);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory,
+            ReplyingKafkaTemplate<String, Object, Object> replyingKafkaTemplate
+    ) {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setReplyTemplate(kafkaTemplate);
-        factory.setConsumerFactory(consumerFactory());
+        factory.setReplyTemplate(replyingKafkaTemplate);
+        factory.setConsumerFactory(consumerFactory);
         return factory;
     }
 }
--- src/main/java/ace/charitan/donation/external/consumer/KafkaMessageConsumer.java
+++ src/main/java/ace/charitan/donation/external/consumer/KafkaMessageConsumer.java
@@ -1,34 +1,54 @@
 package ace.charitan.donation.external.consumer;
 
 
-import ace.charitan.common.dto.donation.DonationDto;
-import ace.charitan.common.dto.donation.GetDonationByIdDto;
-import ace.charitan.common.dto.donation.UpdateDonationStripeIdDto;
+import ace.charitan.common.dto.donation.*;
 import ace.charitan.donation.external.dto.ExternalDonationDto;
 import ace.charitan.donation.external.service.ExternalDonationService;
 import ace.charitan.donation.internal.dto.UpdateDonationRequestDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 class KafkaMessageConsumer {
-    @Autowired
-    private ExternalDonationService service;
+    final private ExternalDonationService service;
+    final private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @KafkaListener(topics = "update-donation-stripe-id", groupId = "donation")
+    private KafkaMessageConsumer(
+            ExternalDonationService service
+    ) {
+        this.service = service;
+    }
+
+    @KafkaListener(topics = "update-donation-stripe-id")
     public void handleUpdateDonationStripeId(UpdateDonationStripeIdDto dto) {
         UpdateDonationRequestDto updateDto = new UpdateDonationRequestDto(null, null, dto.getTransactionStripeId());
 
         service.updateDonation(dto.getId(), updateDto);
     }
 
-    @KafkaListener(topics = "donation-info", groupId = "donation")
+    @KafkaListener(topics = "donation.get.id")
     @SendTo
-    public DonationDto handleGetDonationDto(GetDonationByIdDto reqDto) {
+    public DonationDto handleGetDonationById(GetDonationByIdDto reqDto) {
+        logger.info("Received request for getting donation by id #{}", reqDto.getId());
         ExternalDonationDto dto = service.getDonationById(reqDto.getId());
         return new DonationDto(dto.getId(), dto.getAmount(), dto.getMessage(), dto.getTransactionStripeId(), dto.getProjectId(), dto.getDonorId(),dto.getCreatedAt());
     }
 
+    @KafkaListener(topics = "donation.get.projectId")
+    @SendTo
+    public DonationsDto handleGetDonationByProjectId(GetDonationsByProjectIdDto reqDto) {
+        logger.info("Received request for getting donation by project id #{}", reqDto.id());
+        List<ExternalDonationDto> dtos = service.getDonationByProjectId(reqDto.id());
+        logger.debug("{}", dtos.size());
+        return new DonationsDto(
+                dtos.stream().map(dto -> new DonationDto(
+                        dto.getId(), dto.getAmount(), dto.getMessage(), dto.getTransactionStripeId(), dto.getProjectId(), dto.getDonorId(),dto.getCreatedAt()
+                )).toList()
+        );
+    }
 }
